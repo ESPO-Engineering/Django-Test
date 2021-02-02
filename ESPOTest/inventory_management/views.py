b9bc850ccf32326c90d962dbf2656e3b62d1d0ab
Xhoi Caveli <77589170+xhoic@users.noreply.github.com>
2021-02-02 01:37:02 -0600

    1- Add products through a csv file 2- View added products (fetch data from the relevant table)

--- ESPOTest/inventory_management/views.py
+++ ESPOTest/inventory_management/views.py
@@ -1,7 +1,51 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib import messages
+from .models import Inventory
+import csv, io
+
+
+def buyProducts(request):
+    return render(request, "buy_products.html")
+
+
+def viewProducts(request):
+    template = 'view_products.html'
+    obj = Inventory.objects.all()
+    return render(request, template, {'obj': obj})
+
 
 def home(request):
-    # Your code here!
-    return render(request, "home.html")
+    return render(request, 'home.html')
 
-# Feel free to make more views as needed
+
+def navBar(request):
+    return render(request, 'nav.html')
+
+
+def addProducts(request):
+    template = 'add_products.html'
+    if request.method == "GET":
+        return render(request, template)
+
+    csv_file = request.FILES['file']
+
+    if not csv_file.name.endswith('.csv'):
+        messages.error(request, 'This is not a csv file')
+
+    data_set = csv_file.read().decode('UTF-8')
+    io_string = io.StringIO(data_set)
+    next(io_string)
+
+    for column in csv.reader(io_string, delimiter=',', quotechar="|"):
+        _, created = Inventory.objects.update_or_create(
+            index=column[0],
+            item_name=column[1],
+            quantity=column[2],
+            sell_price=column[3],
+            buy_price=column[4]
+        )
+
+    context = {}
+
+    return render(request, template, context)
